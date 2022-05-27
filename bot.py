5767e31c3d8e955788b8d0094951ed5a0c4d3e3f
ella_ent <entella19@gmail.com>
2022-05-27 04:59:49 +0300

    final for demo

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ from aiogram.utils import executor
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
-from aiogram.utils.callback_data import CallbackData
 
 import asyncio
 import aiohttp
@@ -24,8 +23,9 @@ from bot_utils import (
     get_img_weather_url,
     WEATHER_UNITS,
     metric_cb,
+    details_cb,
     concat_imgs_by_urls,
-    get_menu_buttons
+    get_menu_buttons,
 )
 
 logging.basicConfig(level=logging.INFO)
@@ -129,11 +129,14 @@ async def current_weather_city(callback_query: types.CallbackQuery):
         message_id=callback_query.message.message_id,
         text="Type city",
     )
+    return await bot.answer_callback_query(callback_query_id=callback_query.id)
 
 
-# cancels state handler: need to be done for all text messages with states
 @dp.message_handler(state="*", commands=["cancel"])
 async def cancel_handler(message: types.Message, state: FSMContext):
+    """
+    Cancels any state
+    """
     current_state = await state.get_state()
     if current_state is None:
         return
@@ -157,11 +160,16 @@ async def process_city(message: types.Message, state: FSMContext):
         "APPID": OWM_TOKEN,
     }
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url=OWM_API_URL_FIND, params=params) as response:
-            r = await response.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=OWM_API_URL_FIND, params=params) as response:
+                r = await response.json()
 
-    city = r["list"][0]
+        city = r["list"][0]
+    except IndexError:
+        return await bot.send_message(
+            chat_id=message.chat.id, text="Sorry, no city found"
+        )
 
     img = await get_img_weather_url(
         city["name"],
@@ -172,7 +180,7 @@ async def process_city(message: types.Message, state: FSMContext):
     caption = (
         "Current temp in {city} is {degrees:.0f} {metric} \n"
         "*{description}*\n"
-        "Feels like {feels} {metric}".format(
+        "Feels like {feels} {metric}\n".format(
             city=city["name"],
             degrees=city["main"]["temp"],
             metric=WEATHER_UNITS[units][1],
@@ -181,11 +189,22 @@ async def process_city(message: types.Message, state: FSMContext):
         )
     )
 
+    kb = types.InlineKeyboardMarkup()
+    kb.add(
+        types.InlineKeyboardButton(
+            text="Details",
+            callback_data=details_cb.new(
+                lat=city["coord"]["lat"], lon=city["coord"]["lon"], city=city["name"]
+            ),
+        )
+    )
+
     await bot.send_photo(
         chat_id=message.chat.id,
         photo=img["url"],
         caption=caption,
         parse_mode="markdown",
+        reply_markup=kb,
     )
 
     location = {
@@ -237,11 +256,22 @@ async def current_weather_location(callback_query: types.CallbackQuery):
             )
         )
 
+        kb = types.InlineKeyboardMarkup()
+        kb.add(
+            types.InlineKeyboardButton(
+                text="Details",
+                callback_data=details_cb.new(
+                    lat=r["coord"]["lat"], lon=r["coord"]["lon"], city=r["name"]
+                ),
+            )
+        )
+
         await bot.send_photo(
             chat_id=callback_query.message.chat.id,
             photo=img["url"],
             caption=caption,
             parse_mode="markdown",
+            reply_markup=kb,
         )
     else:
         await LocationForm.location.set()
@@ -250,6 +280,7 @@ async def current_weather_location(callback_query: types.CallbackQuery):
             message_id=callback_query.message.message_id,
             text="Send location",
         )
+    return await bot.answer_callback_query(callback_query_id=callback_query.id)
 
 
 @dp.message_handler(state=LocationForm.location, content_types=["location"])
@@ -291,16 +322,96 @@ async def process_geo(message: types.Message, state: FSMContext):
         )
     )
 
+    kb = types.InlineKeyboardMarkup()
+    kb.add(
+        types.InlineKeyboardButton(
+            text="Details",
+            callback_data=details_cb.new(
+                lat=r["coord"]["lat"], lon=r["coord"]["lon"], city=r["name"]
+            ),
+        )
+    )
+
     await bot.send_photo(
         chat_id=message.chat.id,
         photo=img["url"],
         caption=caption,
         parse_mode="markdown",
+        reply_markup=kb,
     )
 
     location = {"lat": str(lat), "lon": str(lon), "city": r["name"]}
     await db.set_user_location(message.chat.id, location)
-    # await message.reply(f"Current temp in {r['name']}: " + str(r['main']['temp']) + u'\N{DEGREE SIGN}')
+
+
+@dp.callback_query_handler(details_cb.filter())
+async def weather_details(callback_query: types.CallbackQuery, callback_data: dict):
+    lat = callback_data["lat"]
+    lon = callback_data["lon"]
+    city = callback_data["city"]
+
+    units = await db.get_user_metric(callback_query.message.chat.id)
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "units": WEATHER_UNITS[units][0],
+        "exclude": "minutly,hourly",
+        "appid": OWM_TOKEN,
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            url=OWM_API_ONECALL_URL_FORECAST, params=params
+        ) as response:
+            r = await response.json()
+
+    if r["current"]["uvi"] > 2:
+        uvi_description = "*UV index is heightended*"
+    else:
+        uvi_description = ""
+
+    if r["current"]["wind_speed"] <= 5:
+        wind_speed_description = "(Gentle breeze)"
+    elif r["current"]["wind_speed"] <= 8:
+        wind_speed_description = "(Moderate breeze)"
+    elif r["current"]["wind_speed"] <= 11:
+        wind_speed_description = "(Fresh breeze)"
+    else:
+        wind_speed_description = "*Strong breeze*"
+
+    brief = (
+        "Current temp in {city} is {degrees:.0f} {metric} \n"
+        "*{description}*\n"
+        "Feels like {feels} {metric}".format(
+            city=city,
+            degrees=r["current"]["temp"],
+            metric=WEATHER_UNITS[units][1],
+            description=r["current"]["weather"][0]["description"].capitalize(),
+            feels=r["current"]["feels_like"],
+        )
+    )
+    details = (
+        "\nPressure: {pressure} hPa\n"
+        "Humidity: {humidity}%\n"
+        "UV index: {uvi} {uvi_description}\n"
+        "Wind speed: {wind_speed}m/s {wind_speed_description}".format(
+            pressure=r["current"]["pressure"],
+            humidity=r["current"]["humidity"],
+            uvi=r["current"]["uvi"],
+            uvi_description=uvi_description,
+            wind_speed=r["current"]["wind_speed"],
+            wind_speed_description=wind_speed_description,
+        )
+    )
+
+    new_caption = brief + details
+    await bot.edit_message_caption(
+        chat_id=callback_query.message.chat.id,
+        message_id=callback_query.message.message_id,
+        caption=new_caption,
+        parse_mode="markdown",
+    )
+    return await bot.answer_callback_query(callback_query_id=callback_query.id)
 
 
 @dp.message_handler(text=["Settings"])
@@ -358,7 +469,10 @@ async def settings(message: types.Message):
     )
 
 
-@dp.callback_query_handler(lambda query: query.data.startswith("change_location"))
+@dp.callback_query_handler(
+    lambda query: query.data.startswith("change_location")
+    or query.data.startswith("add_location")
+)
 async def change_location(callback_query: types.CallbackQuery):
     kb = types.InlineKeyboardMarkup()
     kb.add(
@@ -379,6 +493,7 @@ async def change_location(callback_query: types.CallbackQuery):
         text="Choose option to set new location",
         reply_markup=kb,
     )
+    return await bot.answer_callback_query(callback_query_id=callback_query.id)
 
 
 @dp.callback_query_handler(lambda query: query.data.startswith("change_weather_city"))
@@ -389,6 +504,7 @@ async def change_weather_city(callback_query: types.CallbackQuery):
         message_id=callback_query.message.message_id,
         text="Type city",
     )
+    return await bot.answer_callback_query(callback_query_id=callback_query.id)
 
 
 @dp.message_handler(state=CitySaveForm.city)
@@ -401,11 +517,17 @@ async def process_change_city(message: types.Message, state: FSMContext):
     s_city = data["city"]
     params = {"q": s_city, "type": "like", "APPID": OWM_TOKEN}
 
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url=OWM_API_URL_FIND, params=params) as response:
-            r = await response.json()
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=OWM_API_URL_FIND, params=params) as response:
+                r = await response.json()
 
-    city = r["list"][0]
+        city = r["list"][0]
+    except IndexError:
+        return await bot.send_message(
+            chat_id=message.chat.id, text="Sorry, no city found"
+        )
+
     location = {
         "lat": str(city["coord"]["lat"]),
         "lon": str(city["coord"]["lon"]),
@@ -427,6 +549,7 @@ async def change_weather_location(callback_query: types.CallbackQuery):
         message_id=callback_query.message.message_id,
         text="Send location",
     )
+    return await bot.answer_callback_query(callback_query_id=callback_query.id)
 
 
 @dp.message_handler(state=LocationSaveForm.location, content_types=["location"])
@@ -511,6 +634,7 @@ async def change_metric(callback_query: types.CallbackQuery, callback_data: dict
         text=msg_text,
         reply_markup=kb,
     )
+    return await bot.answer_callback_query(callback_query_id=callback_query.id)
 
 
 @dp.message_handler(text=["Weather forecast"])
@@ -564,6 +688,7 @@ async def forecast_location(callback_query: types.CallbackQuery):
             message_id=callback_query.message.message_id,
             text="Preparing your weather forecast...",
         )
+        await bot.answer_callback_query(callback_query_id=callback_query.id)
 
         lon = query_data[3]
         lat = query_data[5]
@@ -588,7 +713,7 @@ async def forecast_location(callback_query: types.CallbackQuery):
                 break
             time = datetime.datetime.fromtimestamp(int(day["dt"]))
             img = await get_img_weather_url(
-                weather=str(day["temp"]["day"]),
+                weather=str(round(day["temp"]["day"])),
                 city=f"{time:%Y-%m-%d}",
                 weather_id=str(day["weather"][0]["id"]),
                 metric=WEATHER_UNITS[units][1],
@@ -644,6 +769,10 @@ async def forecast_location_new(message: types.Message, state: FSMContext):
     lat = message.location.latitude
     lon = message.location.longitude
 
+    await bot.send_message(
+        text="Preparing your weather forecast...", chat_id=message.chat.id
+    )
+
     units = await db.get_user_metric(message.chat.id)
     params = {
         "lat": lat,
@@ -655,7 +784,7 @@ async def forecast_location_new(message: types.Message, state: FSMContext):
 
     async with aiohttp.ClientSession() as session:
         async with session.get(
-                url=OWM_API_ONECALL_URL_FORECAST, params=params
+            url=OWM_API_ONECALL_URL_FORECAST, params=params
         ) as response:
             r = await response.json()
 
@@ -665,7 +794,7 @@ async def forecast_location_new(message: types.Message, state: FSMContext):
             break
         time = datetime.datetime.fromtimestamp(int(day["dt"]))
         img = await get_img_weather_url(
-            weather=str(day["temp"]["day"]),
+            weather=str(round(day["temp"]["day"])),
             city=f"{time:%Y-%m-%d}",
             weather_id=str(day["weather"][0]["id"]),
             metric=WEATHER_UNITS[units][1],
@@ -700,16 +829,121 @@ async def forecast_location_new(message: types.Message, state: FSMContext):
     bio.close()
     await bot.delete_message(
         chat_id=message.chat.id,
-        message_id=message.message_id,
+        message_id=message.message_id + 1,
     )
 
     location = {"lat": str(lat), "lon": str(lon), "city": r["timezone"]}
     await db.set_user_location(message.chat.id, location)
 
 
-@dp.message_handler(commands=["test"])
-async def process_start_command(message: types.Message):
-    await message.answer(text="murmur ^3")
+@dp.callback_query_handler(lambda query: query.data.startswith("forecast_city"))
+async def forecast_city(callback_query: types.CallbackQuery):
+    await ForecastCityForm.city.set()
+    await bot.edit_message_text(
+        chat_id=callback_query.message.chat.id,
+        message_id=callback_query.message.message_id,
+        text="Type city",
+    )
+    return await bot.answer_callback_query(callback_query_id=callback_query.id)
+
+
+@dp.message_handler(state=ForecastCityForm.city)
+async def forecast_city_process(message: types.Message, state: FSMContext):
+
+    async with state.proxy() as data:
+        data["city"] = message.text
+    await state.finish()
+
+    await bot.send_message(
+        text="Preparing your weather forecast...", chat_id=message.chat.id
+    )
+
+    units = await db.get_user_metric(message.chat.id)
+    params = {
+        "q": data["city"],
+        "type": "like",
+        "units": WEATHER_UNITS[units][0],
+        "APPID": OWM_TOKEN,
+    }
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url=OWM_API_URL_FIND, params=params) as response:
+                r = await response.json()
+
+        city = r["list"][0]
+    except IndexError:
+        await bot.delete_message(
+            chat_id=message.chat.id,
+            message_id=message.message_id + 1,
+        )
+        return await bot.send_message(
+            chat_id=message.chat.id, text="Sorry, no city found"
+        )
+
+    params = {
+        "lat": city["coord"]["lat"],
+        "lon": city["coord"]["lon"],
+        "units": WEATHER_UNITS[units][0],
+        "exclude": "minutly,hourly",
+        "appid": OWM_TOKEN,
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+            url=OWM_API_ONECALL_URL_FORECAST, params=params
+        ) as response:
+            r = await response.json()
+
+    forecast_imgs = []
+    for index, day in enumerate(r["daily"]):
+        if index == 7:
+            break
+        time = datetime.datetime.fromtimestamp(int(day["dt"]))
+        img = await get_img_weather_url(
+            weather=str(round(day["temp"]["day"])),
+            city=f"{time:%Y-%m-%d}",
+            weather_id=str(day["weather"][0]["id"]),
+            metric=WEATHER_UNITS[units][1],
+        )
+        forecast_imgs.append(img["url"])
+
+    img = await concat_imgs_by_urls(forecast_imgs, bg_color=(134, 185, 224))
+
+    bio = BytesIO()
+    bio.name = str(message.chat.id) + ".png"
+    img.save(bio, "PNG")
+    bio.seek(0)
+
+    alerts = ""
+    if "alerts" in r:
+        caption = "*National alerts*:\n"
+        for alert in r["alerts"]:
+            start = datetime.datetime.fromtimestamp(int(alert["start"]))
+            end = datetime.datetime.fromtimestamp(int(alert["end"]))
+            alerts = alerts + "{start} - {end}:\n {description}\n".format(
+                start=f"{start:%m-%d %H:%M:%S}",
+                end=f"{end:%m-%d %H:%M:%S}",
+                description=alert["description"].replace("*", "\\*"),
+            )
+        alerts = caption + alerts
+    await bot.send_photo(
+        chat_id=message.chat.id,
+        photo=bio,
+        caption=alerts,
+        parse_mode="markdown",
+    )
+    bio.close()
+    await bot.delete_message(
+        chat_id=message.chat.id,
+        message_id=message.message_id + 1,
+    )
+
+    location = {
+        "lat": str(city["coord"]["lat"]),
+        "lon": str(city["coord"]["lat"]),
+        "city": city["name"],
+    }
+    await db.set_user_location(message.chat.id, location)
 
 
 if __name__ == "__main__":
